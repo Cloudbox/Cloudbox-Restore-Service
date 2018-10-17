d9d39db6a09cdddf5240d43c7f07b0690a6da38b
James <l3uddz@gmail.com>
2018-10-17 22:48:51 +0100

    extra excludes

--- scripts/backup_to_google.sh
+++ scripts/backup_to_google.sh
@@ -2,5 +2,5 @@
 
 if ! /usr/bin/screen -list | /bin/grep -q "backup"; then
         #/bin/rm -rfv /root/backup.log
-        /usr/bin/screen -dmS backup /usr/bin/rclone copy /opt/cloudbox_restore_service google: --config=/root/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --exclude='**.log' --log-file=/root/backup.log
+        /usr/bin/screen -dmS backup /usr/bin/rclone copy /opt/cloudbox_restore_service google: --config=/root/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --exclude='**.log' --exclude='**.pyc' --exclude='**__pycache__**' --exclude='**.git**' --log-file=/root/backup.log
 fi
