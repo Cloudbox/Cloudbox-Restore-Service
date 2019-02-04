94e5341fa004039b7ff8356209b104fca1de6513
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-02-03 22:47:16 -0600

    Scripts: Slight reformatting of scripts

--- scripts/backup_to_google.sh
+++ scripts/backup_to_google.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
-#  0,4,8,12,16,20 * * * /opt/cloudbox_restore_service/scripts/backup_to_google.sh
+#########################################################################
+# Title:         Cloudbox Restore Service: Backup To GDrive             #
+# Author(s):     l3uddz, desimaniac                                     #
+# URL:           https://github.com/cloudbox/cloudbox                   #
+# Description:   Backup to Google Drive.                                #
+# --                                                                    #
+#         Part of the Cloudbox project: https://cloudbox.works          #
+#########################################################################
+#                   GNU General Public License v3.0                     #
+#########################################################################
+
+# Cron Schedule: 0,4,8,12,16,20 * * * /opt/cloudbox_restore_service/scripts/backup_to_google.sh
 
 if ! /usr/bin/screen -list | /bin/grep -q "backup"; then
-        #/bin/rm -rfv /root/backup.log
-        /usr/bin/screen -dmS backup bash -c 'cd /opt/cloudbox_restore_service; /usr/bin/zip -qr /root/latest_backup.zip * -x *.log* -x *.pyc* -x *__pycache__*; LAST_BACKUP_TIME="$(/usr/bin/rclone lsl google:/latest_backup.zip | sed -e '"'"'s/^[ \t]*//'"'"' | cut -d '"'"' '"'"' -f 2,3 | cut -d '"'"'.'"'"' -f 1 | sed s/'"'"' '"'"'/_/g | sed s/'"'"':'"'"'/./g)"; /usr/bin/rclone moveto google:latest_backup.zip "google:/archives/$LAST_BACKUP_TIME.zip" --config=/root/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --log-file=/root/backup.log; /usr/bin/rclone move /root/latest_backup.zip google: --config=/root/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --exclude="**.log" --exclude="**.pyc" --exclude="**__pycache__**" --exclude="**.git**" --log-file=/root/backup.log'
+  
+  #/bin/rm -rfv /root/backup.log
+  
+  /usr/bin/screen -dmS backup \
+    bash -c \
+      'cd /opt/cloudbox_restore_service; /usr/bin/zip -qr /root/latest_backup.zip * -x *.log* -x *.pyc* -x *__pycache__*; LAST_BACKUP_TIME="$(/usr/bin/rclone lsl google:/latest_backup.zip | sed -e '"'"'s/^[ \t]*//'"'"' | cut -d '"'"' '"'"' -f 2,3 | cut -d '"'"'.'"'"' -f 1 | sed s/'"'"' '"'"'/_/g | sed s/'"'"':'"'"'/./g)"; /usr/bin/rclone moveto google:latest_backup.zip "google:/archives/$LAST_BACKUP_TIME.zip" --config=/root/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --log-file=/root/backup.log; /usr/bin/rclone move /root/latest_backup.zip google: --config=/root/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --exclude="**.log" --exclude="**.pyc" --exclude="**__pycache__**" --exclude="**.git**" --log-file=/root/backup.log'
 fi
--- scripts/restore_from_google.sh
+++ scripts/restore_from_google.sh
@@ -1,6 +1,29 @@
 #!/usr/bin/env bash
+#########################################################################
+# Title:         Cloudbox Restore Service: Restore Backup From GDrive   #
+# Author(s):     l3uddz, desimaniac                                     #
+# URL:           https://github.com/cloudbox/cloudbox                   #
+# Description:   Restore backup from Google Drive.                      #
+# --                                                                    #
+#         Part of the Cloudbox project: https://cloudbox.works          #
+#########################################################################
+#                   GNU General Public License v3.0                     #
+#########################################################################
 
 if ! /usr/bin/screen -list | /bin/grep -q "restore"; then
-        /bin/rm -rfv /root/restore.log
-        /usr/bin/screen -dmS restore /usr/bin/rclone sync google: /opt/cloudbox_restore_service --config=/root/.config/rclone/rclone.conf --verbose=1 --transfers=8 --stats=60s --checkers=16 --drive-chunk-size=128M --fast-list --log-file=/root/restore.log
+  
+  /bin/rm -rfv /root/restore.log
+  
+  /usr/bin/screen -dmS restore \
+    /usr/bin/rclone sync \
+      --config=/root/.config/rclone/rclone.conf \
+      --verbose=1 \
+      --transfers=8 \
+      --stats=60s \
+      --checkers=16 \
+      --drive-chunk-size=128M \
+      --fast-list \
+      --log-file=/root/restore.log \
+      google: /opt/cloudbox_restore_service
+
 fi
