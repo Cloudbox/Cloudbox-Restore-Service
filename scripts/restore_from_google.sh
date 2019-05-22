6efc8236be6782f48a38a27f23e67db828882c83
desimaniac <5501908+desimaniac@users.noreply.github.com>
2019-05-21 23:20:20 -0500

    Scripts: Added UA and reformatted.

--- scripts/restore_from_google.sh
+++ scripts/restore_from_google.sh
@@ -11,19 +11,20 @@
 #########################################################################
 
 if ! /usr/bin/screen -list | /bin/grep -q "restore"; then
-  
-  /bin/rm -rfv /root/restore.log
-  
-  /usr/bin/screen -dmS restore \
-    /usr/bin/rclone sync \
-      --config=/root/.config/rclone/rclone.conf \
-      --verbose=1 \
-      --transfers=8 \
-      --stats=60s \
-      --checkers=16 \
-      --drive-chunk-size=128M \
-      --fast-list \
-      --log-file=/root/restore.log \
-      google: /opt/cloudbox_restore_service
+
+    /bin/rm -rfv /root/restore.log
+
+    /usr/bin/screen -dmS restore \
+        /usr/bin/rclone sync \
+            --user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36' \
+            --config=/root/.config/rclone/rclone.conf \
+            --verbose=1 \
+            --transfers=8 \
+            --stats=60s \
+            --checkers=16 \
+            --drive-chunk-size=128M \
+            --fast-list \
+            --log-file=/root/restore.log \
+            google: /opt/cloudbox_restore_service
 
 fi
